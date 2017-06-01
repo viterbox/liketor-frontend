62569358533d20130bff7608ec0f4650be9213bb
franco viterbo <francoviterbo@gmail.com>
2017-06-01 14:35:26 -0500

    Iterator map key warning fixed

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -4,12 +4,12 @@ import Movie from './Movie';
 
 function MovieList({ movies }) {
   return (
-    <ul>{movies.map(movie => <Movie title={movie.title} />)}</ul>
+    <ul>{movies.map(movie => <Movie key={movie.id} title={movie.title} />)}</ul>
   );
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf.isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieList;
