7c906218e54cfefcfb6eb4df3b00cb176db6adf4
franco viterbo <francoviterbo@gmail.com>
2017-06-01 00:31:49 -0500

    Main component added

---
+++ src/components/Main.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import MovieList from './movies/MovieList';
+
+class Main extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies: [
+				{ id: 1, title: 'Guardians of the Galaxy Vol. 2' },
+				{ id: 2, title: 'Wonder Woman' },
+			],
+		};
+	}
+
+	render() {
+		return (
+			<div><MovieList movies={this.state.movies} /></div>
+		);
+	}
+
+}
+
+export default Main;
