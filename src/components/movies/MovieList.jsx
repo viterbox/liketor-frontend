345a6e4ee76859522df8c5326204d4c785446787
franco viterbo <francoviterbo@gmail.com>
2017-06-01 11:34:02 -0500

    MovieList is a stateless component

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Movie from './Movie';
 
-class MovieList extends Component {
-
-	render() {
-		const { movies } = this.props;
-		return (
-			<ul>{movies.map(movie => <Movie title={movie.title} />)}</ul>
-		);
-	}
+function MovieList({ movies }) {
+  return (
+    <ul>{movies.map(movie => <Movie title={movie.title} />)}</ul>
+  );
 }
 
 MovieList.propTypes = {
-	movies: React.PropTypes.arrayOf.isRequired,
+  movies: React.PropTypes.arrayOf.isRequired,
 };
 
 export default MovieList;
