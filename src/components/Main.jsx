3d032439eeb21a7c6e9380aadb3581fe6a81033f
franco viterbo <francoviterbo@gmail.com>
2017-06-01 11:36:02 -0500

    airbnb style guidelines applied over Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,22 +2,26 @@ import React, { Component } from 'react';
 import MovieList from './movies/MovieList';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [
+        {
+          id: 1,
+          title: 'Guardians of the Galaxy Vol. 2',
+        }, {
+          id: 2,
+          title: 'Wonder Woman',
+        },
+      ],
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			movies: [
-				{ id: 1, title: 'Guardians of the Galaxy Vol. 2' },
-				{ id: 2, title: 'Wonder Woman' },
-			],
-		};
-	}
-
-	render() {
-		return (
-			<div><MovieList movies={this.state.movies} /></div>
-		);
-	}
+  render() {
+    return (
+      <div><MovieList movies={this.state.movies} /></div>
+    );
+  }
 
 }
 
